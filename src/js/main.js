cc2affa7f6659eecd78da1b57356a09333137913
Journée Jérôme <jerome.journee@outlook.be>
2019-11-23 22:00:00 +0100

    :checkered_flag: Finish Project

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 console.log("Hello");
 
-document.querySelector("body").style.backgroundColor = 'red';
+document.querySelector("html").className = "js-enabled";
+
+window.addEventListener("keypress", son);
+
+function son(event) {
+    const audio = document.querySelector(`audio[data-key="${event.key}"]`);
+    const key = document.querySelector(`.key[data-key="${event.key}"]`);
+
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(event) {
+    if(event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+
